220f92224079e33f24321b68a44ec9264c942fc2
Jinjie Li <lijinjie362@outlook.com>
2023-04-19 22:10:58 +0800

    draw two figs

---
+++ figs/draw_throttle_est_fig.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the data
+data = np.load("/home/lijinjie/ljj_ws/src/oop_qd_onbd/figs/hv_est.npy")
+
+plt.figure(figsize=(3.5, 2.5))
+plt.plot(data[:, 0], data[:, 1], "-", color="#0072BD")
+plt.xlabel("time t [s]", fontsize=8)
+plt.ylabel("gamma", fontsize=8)
+plt.tick_params(axis="both", which="major", labelsize=8)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("hover_estimator.svg", bbox_inches="tight")
+plt.show()
--- figs/draw_training_fig.py
+++ figs/draw_training_fig.py
@@ -7,14 +7,14 @@ train_loss = np.array([2.2060, 1.5871, 1.2323, 1.0925, 1.0254, 1.0183, 1.0198, 1
 test_loss = np.array([2.1179, 1.5315, 1.2072, 1.0787, 1.0170, 1.0106, 1.0129, 1.0137, 1.0132, 1.0163, 1.0213])
 
 plt.figure(figsize=(3.5, 2.5))
-plt.plot(epoch, train_loss, "*-", label="Training Loss", color="blue")
-plt.plot(epoch, test_loss, ".-", label="Test Loss", color="red")
+plt.plot(epoch, train_loss, "*-", label="Training Loss", color="#0072BD")
+plt.plot(epoch, test_loss, ".-", label="Test Loss", color="#D95319")
 plt.xlabel("Epoch", fontsize=8)
 plt.ylabel("Loss", fontsize=8)
 plt.tick_params(axis="both", which="major", labelsize=8)
 plt.grid(True)
 plt.legend(loc="upper right", fontsize=8)
 plt.tight_layout()
-plt.show()
+plt.savefig("training_loss_for_disturb_nn.svg", bbox_inches="tight")
 
-plt.savefig("training_loss_for_disturb_nn.pdf", dpi=300, bbox_inches="tight")
+plt.show()
